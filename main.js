bf77b949041c9c240ca717878932952f02bc8606
chuans <juaazn.05@gmail.com>
2024-05-10 19:45:40 +0200

    js

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
- // Función para guardar los datos en el Local Storage
- function guardarDatos(event) {
+function guardarDatos(event) {
     event.preventDefault(); // Evitar que el formulario se envíe
     const nombre = document.getElementById('nombre').value;
     const edad = document.getElementById('edad').value;
     const mensaje = document.getElementById('mensaje').value;
 
-    // Guardar los datos en el Local Storage
     localStorage.setItem('nombre', nombre);
     localStorage.setItem('edad', edad);
     localStorage.setItem('mensaje', mensaje);
 
-    // Mostrar los datos en el DOM
     mostrarDatos();
 }
 
-// Función para mostrar los datos almacenados en el Local Storage
 function mostrarDatos() {
     const datosDiv = document.getElementById('datos');
     datosDiv.innerHTML = `<p>Nombre: ${localStorage.getItem('nombre')}</p>
@@ -22,19 +18,15 @@ function mostrarDatos() {
                           <p>Mensaje: ${localStorage.getItem('mensaje')}</p>`;
 }
 
-// Función para eliminar los datos del Local Storage
 function eliminarDatos() {
     localStorage.removeItem('nombre');
     localStorage.removeItem('edad');
     localStorage.removeItem('mensaje');
-    mostrarDatos(); // Limpiar los datos mostrados en el DOM
+    mostrarDatos();
 }
 
-// Agregar event listener al formulario para guardar los datos
 document.getElementById('formulario').addEventListener('submit', guardarDatos);
 
-// Mostrar los datos al cargar la página
 mostrarDatos();
 
-// Agregar event listener al botón de eliminar
 document.getElementById('eliminar').addEventListener('click', eliminarDatos);
